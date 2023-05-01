5eb666c6a484f511586e8d0350aa5512b4749ea9
David Laiymani <david.laiymani@icloud.com>
2023-05-01 09:54:57 +0200

    Initial Commit

---
+++ PDFGpt/PDFGptApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFGptApp.swift
+//  PDFGpt
+//
+//  Created by David Laiymani on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFGptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
